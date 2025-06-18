d6cd20afa9232fd93be7de912d6c7b121b1c423a
chancilier1605 <chancelierf246@gmail.com>
2025-06-18 16:05:03 -0400

    Update output.tf

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,5 +1,1 @@
-# Critical for GitHub Actions deployment
-output "bucket_arn" {
-  description = "ARN of the S3 bucket for IAM permissions"
-  value       = arn:aws:iam::002184381968:role/GitHubActionDeployRole
-}
+
