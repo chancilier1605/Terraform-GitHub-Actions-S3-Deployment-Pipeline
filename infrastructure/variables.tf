cd0be7799f3177e3d1be88fd88d6a02e1ef06673
chancilier1605 <chancelierf246@gmail.com>
2025-06-18 15:51:22 -0400

    Update variables.tf

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,13 +1,13 @@
 variable "aws_region" {
-  description = "AWS region to deploy resources"
+  description = "AWS region for resources"
   type        = string
   default     = "us-east-1"
 }
 
 variable "app_name" {
-  description = "Name of the application"
+  description = "Application name (for resource naming)"
   type        = string
-  default     = "my-app"
+  default     = "myapp"
 }
 
 variable "environment" {
@@ -16,42 +16,8 @@ variable "environment" {
   default     = "dev"
 }
 
-variable "tags" {
-  description = "Common tags for all resources"
-  type        = map(string)
-  default = {
-    Terraform   = "true"
-    Environment = "dev"
-    Project     = "my-app"
-  }
-}
-
-variable "enable_website_hosting" {
-  description = "Enable S3 static website hosting"
-  type        = bool
-  default     = true
-}
-
-variable "index_document" {
-  description = "Default index document for website"
+variable "terraform_state_bucket" {
+  description = "Existing S3 bucket for Terraform state"
   type        = string
-  default     = "index.html"
-}
-
-variable "error_document" {
-  description = "Default error document for website"
-  type        = string
-  default     = "error.html"
-}
-
-variable "enable_bucket_versioning" {
-  description = "Enable versioning for the S3 bucket"
-  type        = bool
-  default     = true
-}
-
-variable "block_public_access" {
-  description = "Block public access to the S3 bucket"
-  type        = bool
-  default     = false
+  default     = "terraform-s3-staticwebsite" 
 }
